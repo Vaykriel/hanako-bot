41839e9bb84870d56d04a03cfb2c5ac1c6fc6e2a
unknown <yuzudook@gmail.com>
2019-08-10 09:55:10 +0700

    Tes naikin bitrates

--- index.js
+++ index.js
@@ -408,7 +408,7 @@ function play(guild, song) {
 	}
 	console.log(serverQueue.songs);
 
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url,{filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1024 * 1024 * 10 })
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url,{filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1024 * 1024 * 10 }))
 		.on('end', reason => {
 			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
 			else console.log(reason);
