d1bf7b49a3104f33b0c9a3de12242701073eb407
Da <axvylbebek@gmail.com>
2020-08-26 05:00:55 +0700

    Minor Fix

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ client.on('message', async msg => {
  const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(msg.guild.id);
 
 if(msg.guild.id=='729296478337761292'){
 	
